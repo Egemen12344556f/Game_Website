88f91f55d23e2337d8f13f9cc7d1c1e4253943cd
Egemen12344556f <112466102+Egemen12344556f@users.noreply.github.com>
2024-02-21 18:10:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,19 @@
-from flask import Flask, request
-import os
+from flask import request, render_template
 
-app = Flask(__name__)
-
-# Define the directory where you want to save the files
-SAVE_DIR = 'saved_forms'
-
-# Check if the directory exists, if not create it
-if not os.path.exists(SAVE_DIR):
-    os.makedirs(SAVE_DIR)
-
-@app.route('/')
-def index():
-    return open('templates/contact.html').read()
-
-@app.route('/submit', methods=['POST'])
 def submit():
     name = request.form['name']
     email = request.form['email']
     message = request.form['message']
 
-    # Save data to a file in the specified directory
-    file_path = os.path.join(SAVE_DIR, f'{name}.txt')
-    with open(file_path, 'w') as file:
+    # Write form data to a file
+    with open('form_data.txt', 'a') as file:
         file.write(f'Name: {name}\n')
         file.write(f'Email: {email}\n')
-        file.write(f'Message: {message}')
+        file.write(f'Message: {message}\n')
+        file.write('\n')  # Add a new line to separate entries
+        
+    # Return a response indicating successful form submission
+    return render_template('contactalert.html')
 
-    return open('templates/contactalert.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
