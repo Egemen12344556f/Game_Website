b4563eb56310523819483979f739a7fa8f5cbb04
Egemen12344556f <112466102+Egemen12344556f@users.noreply.github.com>
2024-02-21 18:35:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
-from save import submit
-
-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+def submit():
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+
+    # Write form data to a file
+    with open('form_data.txt', 'a') as file:
+        file.write(f'Name: {name}\n')
+        file.write(f'Email: {email}\n')
+        file.write(f'Message: {message}\n')
+        file.write('\n')  # Add a new line to separate entries
+        
+    # Return a response indicating successful form submission
+    return render_template('contactalert.html')
 
 @app.route('/')
 def index():
